7eadc4069615f3a4f55eb0fb9a49941390111bf2
Mackswehl <max.s.vale01@gmail.com>
2017-12-04 18:38:13 -0500

    Created static isSorted method and added a test in the main mathod.

--- 46_ALTester/ALTester.java
+++ 46_ALTester/ALTester.java
@@ -1,11 +1,29 @@
 import java.util.ArrayList;
 
 public class ALTester {
-
+  
   public static void populate ( ArrayList<Comparable> list ) {
     for (int i = 0; i < 23; i++) {
-      list.add((int)(Math.random() * 1000));
+      list.add((int)(Math.random() * 100));
     }
+  }
+
+  public static boolean isSorted (ArrayList<Comparable> list) {
+    boolean ascending = true;
+    boolean descending = true;
+    for (int i = 0; i < list.size() - 1; i++) {
+      if (list.get(i).compareTo(list.get(i+1)) < 0) {
+        ascending = false;
+        break;
+      }
+    }
+    for  (int i = 0; i < list.size() - 1; i++) {
+      if (list.get(i).compareTo(list.get(i+1)) > 0) {
+        descending = false;
+        break;
+      }
+    }
+    return ascending || descending;
   }
 
   public static void main (String[] args) {
@@ -13,7 +31,9 @@ public class ALTester {
     ArrayList<Comparable> test = new ArrayList<Comparable>();
 
     populate(test);
-    System.out.println(test.size());
+    System.out.println(test);
+    System.out.println("\nIs ArrayList test sorted?");
+    System.out.println(isSorted(test));
 
   } // end main method
 
