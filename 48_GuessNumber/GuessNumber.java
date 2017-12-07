0c7faee2a7e91d6b2749878884c21b01c3650140
Mackswehl <max.s.vale01@gmail.com>
2017-12-06 21:00:45 -0500

    Created new java file in new hw folder

---
+++ 48_GuessNumber/GuessNumber.java
@@ -0,0 +1,14 @@
+// Maxwell Vale
+// APCS1 pd02
+// HW48 -- Keep Guessing
+// 2017-12-06
+
+import cs1.Keyboard;
+
+public class GuessNumber {
+
+  public static void main (String[] args) {
+
+  }// end main method
+
+}// end class GuessNumber
