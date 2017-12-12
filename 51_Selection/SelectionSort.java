8dda494233710ea84daedbf034201e0b43381dcf
Mackswehl <max.s.vale01@gmail.com>
2017-12-11 21:46:17 -0500

    Created 51_Selection with SelectionSort.java in it.

---
+++ 51_Selection/SelectionSort.java
@@ -0,0 +1,119 @@
+/***************************************
+* class SelectionSort -- implements SelectionSort algorithm
+***************************************/
+
+import java.util.ArrayList;
+
+public class SelectionSort
+{
+  //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+  //precond: lo < hi && size > 0
+  //postcond: returns an ArrayList of random integers
+  //          from lo to hi, inclusive
+  public static ArrayList populate( int size, int lo, int hi ) {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+      //     offset + rand int on interval [lo,hi]
+      retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+      size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al ) {
+    int randomIndex;
+    for( int i = al.size()-1; i > 0; i-- ) {
+      //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+      //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+  // VOID version of SelectionSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void selectionSortV( ArrayList<Comparable> data )
+  {
+    // for loop to iterate through data, changing the element at index to the smallest element in data
+    for (int index = 0; index < data.size(); index++) {
+      // var holding the smallest value of data; initialized as element at index
+      Comparable min = data.get(index);
+      //for loop used to iterate through data to find the smallest element and set min equal to it
+      for (int i = index + 1; i < data.size(); i++) {
+        if (data.get(i).compareTo(min) < 0) {
+          min = data.get(i);
+        }
+      }
+      //Swapping the next beginning index of data to the smallest element of the remaining part of data
+      Comparable first = data.get(index); // next first element of data
+      int minIndex = data.indexOf(min); // the index of min
+      data.set(index, min); // swap min with the element at the next first index
+      data.set(minIndex, first);
+    }
+  }//end selectionSortV
+
+
+  // ArrayList-returning selectionSort
+  // postcondition: order of input ArrayList's elements unchanged
+  //                Returns sorted copy of input ArrayList.
+  public static ArrayList<Comparable> selectionSort( ArrayList<Comparable> input )
+  {
+    ArrayList<Comparable> copy = new ArrayList<Comparable>();
+    // copying ArrayList input over to ArrayList copy
+    for (int i = 0; i < input.size(); i++) {
+      copy.add(input.get(i));
+    }
+    selectionSortV(copy);
+    return copy;
+  }//end selectionSort
+
+
+  public static void main( String [] args )
+  {
+    /*===============for VOID methods=============
+    ArrayList glen = new ArrayList<Integer>();
+    glen.add(7);
+    glen.add(1);
+    glen.add(5);
+    glen.add(12);
+    glen.add(3);
+    System.out.println( "ArrayList glen before sorting:\n" + glen );
+    selectionSortV(glen);
+    System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+
+    ArrayList coco = populate( 10, 1, 1000 );
+    System.out.println( "ArrayList coco before sorting:\n" + coco );
+    selectionSortV(coco);
+    System.out.println( "ArrayList coco after sorting:\n" + coco );
+    ============================================*/
+
+
+    ArrayList glen = new ArrayList<Integer>();
+    glen.add(7);
+    glen.add(1);
+    glen.add(5);
+    glen.add(12);
+    glen.add(3);
+    System.out.println( "ArrayList glen before sorting:\n" + glen );
+    ArrayList glenSorted = selectionSort( glen );
+    System.out.println( "sorted version of ArrayList glen:\n"
+    + glenSorted );
+    System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+    ArrayList coco = populate( 10, 1, 1000 );
+    System.out.println( "ArrayList coco before sorting:\n" + coco );
+    ArrayList cocoSorted = selectionSort( coco );
+    System.out.println( "sorted version of ArrayList coco:\n"
+    + cocoSorted );
+    System.out.println( "ArrayList coco after sorting:\n" + coco );
+    /*==========for AL-returning methods==========
+    ============================================*/
+
+  }//end main
+
+}//end class SelectionSort
