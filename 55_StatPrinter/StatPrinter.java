612b9a53c44ccb9f88fb05e1b26c310dcc843dfb
Mackswehl <max.s.vale01@gmail.com>
2017-12-18 15:50:46 -0500

    New homework dir and hw file. Coding up the AP Question from class

---
+++ 55_StatPrinter/StatPrinter.java
@@ -0,0 +1,15 @@
+// Maxwell Vale
+// APCS1 pd02
+// HW<hw number> - <HW NAME>
+// <yyyy-mm-dd>
+
+public class StatPrinter {
+
+  private ArrayList<Comparable> _data = new ArrayList<Comparable>();
+  private ArrayList<Comparable> freqList = new ArrayList<Comparable>();
+
+  public static void main (String[] args) {
+
+  } // end main method
+
+} // end class StatPrinter
