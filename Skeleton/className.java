f10be50f2597bf917a7d6355588c060b1c3cf23d
Mackswehl <max.s.vale01@gmail.com>
2018-02-01 20:34:12 -0500

    Organized Homeworks by semester

---
+++ Skeleton/className.java
@@ -0,0 +1,23 @@
+// Maxwell Vale
+// APCS1 pd02
+// HW<hw number> - <HW NAME>
+// <yyyy-mm-dd>
+
+import java.util.ArrayList;
+
+public class className {
+
+  public static void main (String[] args) {
+
+    ArrayList<Integer> vale = new ArrayList<Integer>();
+    for(int i = 0; i < 10; i++) {
+      vale.add(i);
+    }
+
+    for (int i : vale) {
+      System.out.println(i);
+    }
+
+  } // end main method
+
+} // end class className
